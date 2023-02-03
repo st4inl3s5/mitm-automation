93c5cd3ed526a4df619c2e1ef49e7a27447604b1
Yiğit Aydemir <68844502+st4inl3s5@users.noreply.github.com>
2023-02-03 17:22:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ print("\nKurulum baslatiliyor...")
 time.sleep(2)
 
 subprocess.call(["apt-get", "install", "mitmproxy"])
-subprocess.call(["apt-get", "install", "ettercap"])
 subprocess.call(["pip", "install", "colorama"])
 
 print("\nKurulum tamamlandi.")
